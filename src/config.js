a068101d29091c522c9adf306893682854da5d61
shuyossy <shuyossy220@gmail.com>
2024-12-08 17:46:33 +0900

    chore: githubpages用に画像のパスを書き換え

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 // このファイルは座席表画像のパスやマップサイズなどの設定値を管理します。
 
-export const BACKGROUND_IMAGE_URL = "/images/seatmap.png"; 
+export const BACKGROUND_IMAGE_URL = "/seat-reservation-sample/images/seatmap.png"; 
 // 座席表として表示する画像のURL。public/imagesに配置する想定。
 
 export const MAP_WIDTH = 800;  // マップの幅(px)
